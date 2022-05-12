7c1654d59e5ec754b36bbafc11ff26175bece571
noguchitakuya <takuya.noguchi@rakus-partners.co.jp>
2022-05-12 15:41:00 +0900

    ブログ完成

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -2,28 +2,53 @@
 import Head from "next/head";
 import styles from "./layout.module.css";
 import utilsStyles from "../styles/utils.module.css";
+import { FC, ReactNode } from "react";
+import Link from "next/link";
 
 const name = "takuya noguchi";
 export const siteTitle = "Next.js blog";
 
-function Layout({ children }) {
+type props = {
+  children: ReactNode;
+  home?: boolean;
+};
+
+const Layout: FC<props> = ({ children, home }) => {
   return (
     <div className={styles.container}>
       <Head>
         <title>Next.js blog</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <header>
-        <img
-          src="/images/profile.png"
-          alt=""
-          className={utilsStyles.borderCircle}
-        />
-        <h1>{name}</h1>
-      </header>
+      {home ? (
+        <>
+          <header className={styles.header}>
+            <img
+              src="/images/profile.png"
+              alt=""
+              className={`${utilsStyles.borderCircle} ${styles.headerHomeImage}`}
+            />
+            <h1 className={utilsStyles.heading2Xl}>{name}</h1>
+          </header>
+        </>
+      ) : (
+        <header className={styles.header}>
+          <img
+            src="/images/profile.png"
+            alt=""
+            className={`${utilsStyles.borderCircle} ${styles.headerImage}`}
+          />
+          <h1 className={utilsStyles.heading2Xl}>{name}</h1>
+        </header>
+      )}
       <main>{children}</main>
+      {!home && (
+        <div>
+          <Link href="/">←　ホームへ戻る</Link>
+        </div>
+      )}
     </div>
   );
-}
+};
 
 export default Layout;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,66 @@
 import Head from "next/head";
 import Image from "next/image";
-import Layout from "../components/Layout";
+import Link from "next/link";
+import Layout, { siteTitle } from "../components/Layout";
 import styles from "../styles/Home.module.css";
+import utilStyles from "../styles/utils.module.css";
+import { getPostsData } from "../lib/post";
 
-export default function Home() {
+//SSGの場合
+export async function getStaticProps() {
+  const allPostsData = getPostsData();
+  console.log(allPostsData);
+
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+}
+
+// //SSRの場合
+// export async function getServerSideProps(context) {
+//   return {
+//     props: {
+//       //コンポーネントの渡すためのprops
+//     },
+//   };
+// }
+
+export default function Home({ allPostsData }) {
   return (
-    <Layout>
-      <div></div>
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <div>
+        <section className={utilStyles.headingMd}>
+          <p>
+            私はフロントエンドエンジニアです。会社員として働いています。好きな言語はTypeScriptです。
+          </p>
+        </section>
+        <section className={styles.main}>
+          <h2>📝エンジニアのブログ</h2>
+          <div className={styles.grid}>
+            {allPostsData.map(({ id, title, date, thumbnail }) => (
+              <article key={id}>
+                <Link href={`posts/${id}`}>
+                  <img
+                    src={`${thumbnail}`}
+                    alt=""
+                    className={styles.thumbnailImage}
+                  />
+                </Link>
+                <Link href={`posts/${id}`}>
+                  <a className={utilStyles.boldText}>{title}</a>
+                </Link>
+                <br />
+                <small className={utilStyles.lightText}>{date}</small>
+              </article>
+            ))}
+          </div>
+        </section>
+      </div>
     </Layout>
   );
 }
